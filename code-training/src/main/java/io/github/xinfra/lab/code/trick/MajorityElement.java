ffe6ec9835f2ea74298772260506479a61633e73
joecqupt <joe469391363@gmail.com>
2025-03-15 12:35:12 +0800

    update

---
+++ code-training/src/main/java/io/github/xinfra/lab/code/trick/MajorityElement.java
@@ -0,0 +1,19 @@
+package io.github.xinfra.lab.code.trick;
+
+public class MajorityElement {
+
+    class Solution {
+        public int majorityElement(int[] nums) {
+            int count = 0;
+            int candidate = 0;
+            for (int num : nums) {
+                if (count == 0) {
+                    candidate = num;
+                }
+                count = candidate == num ? count + 1 : count - 1;
+            }
+            return candidate;
+        }
+    }
+
+}
